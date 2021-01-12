824893203663fa02d5f5ce4d998512fed6413815
Ali Bas <alibas01@gmail.com>
2021-01-12 15:28:45 -0500

    refined spinner with do while

--- timer1.js
+++ timer1.js
@@ -5,26 +5,32 @@ let time = [];
 for (let num of args) {
   time.push(parseInt(num));
 }
+// time to beep
 for (let item of time) {
+  
   setTimeout(() => {
 
     process.stdout.write('\x07');
+    process.stdout.write('beep');
+    process.stdout.clearLine();
   }, item * 1000);
-  
-}
 
+
+
+//spinner
 let delay = 100;
 let interval = 200;
 const str = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ' ]
 
 do {
-  for (let i = 0; i < 100; i++) {
-    for (let item of str) {
+  //for (let i = 0; i < 100; i++) {
+    for (let itemx of str) {
       setTimeout(() => {
   
-        process.stdout.write(item);
+        process.stdout.write(itemx);
       }, delay += interval);
     
     }
-  }  
-} while (delay < Math.max(time) * 950);
+  //}  
+} while (delay < Math.max(item) * 1000);
+}
